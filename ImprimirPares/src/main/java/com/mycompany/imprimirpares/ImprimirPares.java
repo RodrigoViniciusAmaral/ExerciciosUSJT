19638133d41f162429223e88cc81d5921886f247
RodrigoViniciusAmaral <rodrigo.amaral@totalip.com.br>
2022-10-07 23:16:59 -0300

    ExercicioImprimirPares
    Signed-off-by: RodrigoViniciusAmaral <rodrigo.amaral@totalip.com.br>

---
+++ ImprimirPares/src/main/java/com/mycompany/imprimirpares/ImprimirPares.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.imprimirpares;
+
+/**
+ *
+ * @author rodrigo_pc
+ */
+public class ImprimirPares {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
