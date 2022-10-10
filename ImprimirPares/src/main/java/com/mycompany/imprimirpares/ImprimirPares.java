ecfec7e89178743d757dd265917979c99e4858df
RodrigoViniciusAmaral <rodrigo.amaral@totalip.com.br>
2022-10-10 08:20:49 -0300

    Exercicio ImprimirPares Completo
    Signed-off-by: RodrigoViniciusAmaral <rodrigo.amaral@totalip.com.br>

--- ImprimirPares/src/main/java/com/mycompany/imprimirpares/ImprimirPares.java
+++ ImprimirPares/src/main/java/com/mycompany/imprimirpares/ImprimirPares.java
@@ -10,8 +10,15 @@ package com.mycompany.imprimirpares;
  * @author rodrigo_pc
  */
 public class ImprimirPares {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+     public static void main(String[] args) {
+        int i = 0;
+        while(i <= 50){
+            if(i % 2 == 0){
+                System.out.println(i);
+                i++;
+            } else{
+                i++;
+            }   
+        }
     }
 }
